5533d606db97e98ffd30167b74de5444b379b96a
Patrick Fantato <me@pfantato.space>
2025-03-11 12:14:49 +0000

    feat: add pino as main logger

---
+++ src/utils/logger.util.ts
@@ -0,0 +1,12 @@
+import pino from 'pino'
+
+export const logger = pino({
+  name: '@pfantato/printful-ts',
+  level: process.env.PRINTFUL_LOG_LEVEL || 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+})

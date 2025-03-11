24bd628b734f587200c7b52ffa460c3cdfad60b9
Patrick Fantato <me@pfantato.space>
2025-03-11 12:13:56 +0000

    feat: add privateToken as required in PrintfulConfig type

--- src/schemas/common/printful.schemas.ts
+++ src/schemas/common/printful.schemas.ts
@@ -3,9 +3,13 @@ import { z } from 'zod'
 export const PrintfulVersion = z.enum(['v1', 'v2'])
 export type PrintfulVersion = z.infer<typeof PrintfulVersion>
 
-export const PrintfulConfig = z.object({
-  privateToken: z.string(),
-  baseUrl: z.string().url().optional().default('https://api.printful.com'),
-  version: PrintfulVersion.optional().default(PrintfulVersion.enum.v2),
-})
+export const PrintfulConfig = z
+  .object({
+    privateToken: z.string(),
+    baseUrl: z.string().url().optional().default('https://api.printful.com'),
+    version: PrintfulVersion.optional().default(PrintfulVersion.enum.v2),
+  })
+  .required({
+    privateToken: true,
+  })
 export type PrintfulConfig = z.infer<typeof PrintfulConfig>

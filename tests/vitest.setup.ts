c323ba5503ddacbe43ef55fe9f915167ffc57a1c
Patrick Fantato <me@pfantato.space>
2025-03-24 15:25:05 +0000

    chore: remove msw logger and unnecessary zod-schema-faker initialization

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,23 +1,11 @@
 import { beforeAll, afterEach, afterAll, vi } from 'vitest'
-import { install } from 'zod-schema-faker'
 
 import { mockServer } from './mocks/msw/msw.server'
-import { logger } from '@printful-ts/utils'
 
-install()
-
-mockServer.events.on(
-  'response:mocked',
-  async ({ request, requestId, response }) => {
-    response.headers.set('x-msw-request', requestId)
-    response.headers.set('x-msw-mocked', 'true')
-
-    logger.info(
-      Object.fromEntries(response.headers),
-      `[MSW intercepted request]: HTTP ${response.status} - ${request.method} ${request.url}`,
-    )
-  },
-)
+mockServer.events.on('response:mocked', async ({ requestId, response }) => {
+  response.headers.set('x-msw-request', requestId)
+  response.headers.set('x-msw-mocked', 'true')
+})
 
 global.fetch = fetch
 
